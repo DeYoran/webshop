8f7ad1936f7b83fd2de9d2de9ac0e49b86e4b1f5
Yoran Engelberts <yoran.engelberts@gmail.com>
2013-03-04 11:15:57 +0100

    4-3-13'

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('DB_NAME','webshop');
+	define('DB_USER','webshop_root');
+	define('DB_PASSWORD','');
+	define('DB_HOST','localhost');	
+	define('BASE_URL','Webshop')
+?>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
-echo'mijn eerste mvc page';
+	define('DS', DIRECTORY_SEPARATOR);
+	//echo DS;
+	define('ROOT',dirname(dirname(__file__)));
+	// ROOT;
+	$url = (isset($_GET['url'])) ? $_GET['url'] : 'users/viewall';
+	//echo $url;
+	require_once(ROOT.DS.'library'.DS.'bootstrap.php');
 ?>
